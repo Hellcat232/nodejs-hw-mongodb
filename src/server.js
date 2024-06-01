386a9c083d758c9f25dbb0bc59f16b5cabafc3c1
Hellcat232 <maxkuich1993@gmail.com>
2024-06-01 03:20:47 +0300

    qwerty

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import pino from 'pino-http';
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
-const PORT = Number(env('PORT', '3000'));
+const PORT = Number(env('PORT', 3000));
 
 export const setupServer = () => {
   const app = express();
