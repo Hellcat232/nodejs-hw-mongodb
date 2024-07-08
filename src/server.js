7148a1ce65dba5f9bb180542d42035d56f42343d
Hellcat232 <maxkuich1993@gmail.com>
2024-07-08 22:58:52 +0300

    1234

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-//12312311
+//123123111
 import { env } from './utils/env.js';
 
 import router from './routers/index.js';
